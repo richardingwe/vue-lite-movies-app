37f101010ffaa0b15754661e59f9292f958de3f0
richardingwe <richardingwe5@gmail.com>
2021-07-17 08:32:25 +0100

    added a glass drop filter to image

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -29,7 +29,7 @@
 
 <style>
 	.modalDiv {
-		background: rgba(128, 128, 128, 0.514);
+		/* background: rgba(128, 128, 128, 0.514); */
 		height: 100vh;
 		width: 100vw;
 		position: fixed;
@@ -38,6 +38,16 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		background: linear-gradient(
+			-45deg,
+			rgba(255, 255, 255, 0.1),
+			rgba(255, 255, 255, 0.4)
+		);
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		backdrop-filter: blur(4px);
+		-webkit-backdrop-filter: blur(4px);
+		border-radius: 10px;
+		border: 1px solid rgba(255, 255, 255, 0.18);
 	}
 
 	.header {
